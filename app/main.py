37bb4159ce0534a126ac17dee19c3a54d9315c00
Khalil-Mekki <mekkiikhalil@gmail.com>
2022-01-22 22:45:27 +0000

    wsgi fix

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from flask_restful import Api
 from app.api import app, PerksManager, ClientsManager, PartnerManager, UserManager
 
 
-if __name__ == "__main__":
 
-    api = Api(app)
-    api.add_resource(UserManager, "/api/users")
-    api.add_resource(PerksManager, "/api/perks")
-    api.add_resource(ClientsManager, "/api/clients")
-    api.add_resource(PartnerManager, "/api/partners")
-    app.run(debug=True)
+
+api = Api(app)
+api.add_resource(UserManager, "/api/users")
+api.add_resource(PerksManager, "/api/perks")
+api.add_resource(ClientsManager, "/api/clients")
+api.add_resource(PartnerManager, "/api/partners")
+app.run(debug=True)
